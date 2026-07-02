f5cf02ccbb7b97cdd52dae0ad38fa01839e9db6f
ADEKEYE LUKMAN SEGUN <adekeyelukmana@gmail.com>
2026-07-02 04:08:57 -0700

    Update CORS configuration in main.ts
    Comment out the previous CORS configuration and enable CORS for all origins.

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,18 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-   app.enableCors({
-    origin: [
-      'https://leafs.lovable.app',
-      /\.lovable\.app$/,
-      'http://localhost:5173',
-    ],
-    credentials: true,
-  });
+   // app.enableCors({
+  //   origin: [
+  //     'https://leafs.lovable.app',
+  //     /\.lovable\.app$/,
+  //     'http://localhost:5173',
+  //   ],
+  //   credentials: true,
+  // });
+  app.enableCors({
+  origin: true,
+  credentials: true,
+});
   
   app.useGlobalPipes(
     new ValidationPipe({
